027ac0a98a1b246d3547dd6ff959e1cf4492b884
Nikita <nikitavornik@gmail.com>
2022-04-07 09:41:03 +0300

    make it better

--- budget_prog/__init__.py
+++ budget_prog/__init__.py
@@ -35,7 +35,6 @@ class MyModelView(ModelView):
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'python.vornik@gmail.com'
 app.config['MAIL_PASSWORD'] = password
 app.config['MAIL_USE_TLS'] = False
