dc6a27de85c623bcd7fcaf19332556c25d5e7573
Nikita <nikitavornik@gmail.com>
2022-04-07 11:18:39 +0300

    make it better

--- budget_prog/__init__.py
+++ budget_prog/__init__.py
@@ -38,7 +38,7 @@ app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'python.vornik@gmail.com'
-app.config['MAIL_PASSWORD'] = password
+app.config['MAIL_PASSWORD'] = 'dwemlsghhptnyvfw'
 
 mail = Mail(app)
 
