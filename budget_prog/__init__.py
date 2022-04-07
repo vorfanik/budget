f9f36c59278b7a085982286f36dbf7fccd66e1f1
Nikita <nikitavornik@gmail.com>
2022-04-07 11:02:28 +0300

    make it better

--- budget_prog/__init__.py
+++ budget_prog/__init__.py
@@ -34,11 +34,11 @@ class MyModelView(ModelView):
 
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'python.vornik@gmail.com'
 app.config['MAIL_PASSWORD'] = password
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+
 mail = Mail(app)
 
 from . import routes
@@ -47,6 +47,3 @@ from . import routes
 admin = Admin(app)
 admin.add_view(MyModelView(Entries, db.session))
 admin.add_view(MyModelView(Account, db.session))
-
-
-
